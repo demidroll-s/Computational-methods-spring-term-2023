72dca4b1bd0c6a0cc23a4c7a2a90a35f7c458994
Max Venderevskii <venderevskii.mi@phystech.edu>
2023-04-05 13:51:26 +0300

    1 task done

--- Task_3_FFT/image_converter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <stdio.h>
-#include <fstream>
-
-bool GetImageSize(const std::string& image_filename, int& dim_x, int& dim_y);
-void ConvertImageToArray(const std::string& image_filename, const std::string& array_filename);
